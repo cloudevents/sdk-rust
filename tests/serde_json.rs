46d1c65e2fd57497618ac32cfa7199a2f7bbf270
slinkydeveloper <francescoguard@gmail.com>
2020-03-18 09:08:03 +0100

    Clarified the serde test
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

--- tests/serde_json.rs
+++ tests/serde_json.rs
@@ -6,38 +6,30 @@ use serde_json::Value;
 mod test_data;
 use test_data::*;
 
+/// This test is a parametrized test that uses data from tests/test_data
+/// The test follows the flow Event -> serde_json::Value -> String -> Event
 #[rstest(
     event,
-    json,
+    expected_json,
     case::minimal_v1(minimal_v1(), minimal_v1_json()),
     case::full_v1_no_data(full_v1_no_data(), full_v1_no_data_json()),
     case::full_v1_with_json_data(full_v1_json_data(), full_v1_json_data_json()),
     case::full_v1_with_base64_data(full_v1_binary_data(), full_v1_base64_data_json())
 )]
-fn serialize_deserialize_should_succeed(event: Event, json: Value) {
+fn serialize_deserialize_should_succeed(event: Event, expected_json: Value) {
+    // Event -> serde_json::Value
     let serialize_result = serde_json::to_value(event.clone());
     assert_ok!(&serialize_result);
     let actual_json = serialize_result.unwrap();
-    assert_eq!(&actual_json, &json);
-    let deserialize_result: Result<Event, serde_json::Error> = serde_json::from_value(actual_json);
+    assert_eq!(&actual_json, &expected_json);
+
+    // serde_json::Value -> String
+    let actual_json_serialized = actual_json.to_string();
+    assert_eq!(actual_json_serialized, expected_json.to_string());
+
+    // String -> Event
+    let deserialize_result: Result<Event, serde_json::Error> = serde_json::from_str(&actual_json_serialized);
     assert_ok!(&deserialize_result);
     let deserialize_json = deserialize_result.unwrap();
     assert_eq!(deserialize_json, event)
-}
-
-
-#[cfg(test)]
-mod tests {
-    use cloudevents::EventBuilder;
-    #[test]
-    fn serialize_full_event_to_json_data() {
-        let event = EventBuilder::v10()
-            .id("id")
-            .ty("test type")
-            .source("http://localhost:8080")
-            .build();
-        let deserialized_event = serde_json::to_value(event);
-        let json = "{\"type\":\"test type\",\"specversion\":\"1.0\",\"source\":\"http://localhost:8080\",\"id\":\"id\",\"datacontenttype\":\"application/json\",\"data\":\"\\\"test\\\"\"}";
-        assert_eq!(json, deserialized_event.unwrap());
-    }
 }

57f290a57a01c872ca5a900bf4d44f6f9a837745
slinkydeveloper <francescoguard@gmail.com>
2020-05-19 12:39:26 +0200

    Added reqwest wasm example
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

---
+++ example-projects/reqwest-wasm-example/index.js
@@ -0,0 +1,17 @@
+import $ from 'jquery';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
+import("./pkg").then(rustModule => {
+    $(document).ready(function () {
+        $("#send").click(function () {
+            let target = $("#event_target").val()
+            let ty = $("#event_type").val()
+            let dataContentType = $("#event_datacontenttype").val()
+            let data = $("#event_data").val()
+
+            rustModule.run(target, ty, dataContentType, data).catch(console.error);
+        });
+    })
+}).catch(console.error);

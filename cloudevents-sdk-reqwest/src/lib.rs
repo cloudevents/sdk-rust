8d5192bfcc93c9d4aaaa860fbfe77b83de298eb6
Pranav Bhatt <adpranavb2000@gmail.com>
2020-06-21 01:35:32 +0530

    tested api calls within rust
    Signed-off-by: Pranav Bhatt <adpranavb2000@gmail.com>

--- cloudevents-sdk-reqwest/src/lib.rs
+++ cloudevents-sdk-reqwest/src/lib.rs
@@ -4,6 +4,8 @@ mod client_request;
 mod client_response;
 
 pub use client_request::event_to_request;
+pub use client_request::RequestBuilderExt as RequestBuilderExt;
 pub use client_request::RequestSerializer;
 pub use client_response::response_to_event;
 pub use client_response::ResponseDeserializer;
+pub use client_response::ResponseExt as ResponseExt;

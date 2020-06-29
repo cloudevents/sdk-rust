1d240f6e2bcf166caa9e26525b6a525e198f9bbd
Pranav Bhatt <adpranavb2000@gmail.com>
2020-06-29 11:10:40 +0530

    init
    Signed-off-by: Pranav Bhatt <adpranavb2000@gmail.com>

--- cloudevents-sdk-rdkafka/src/lib.rs
+++ cloudevents-sdk-rdkafka/src/lib.rs
@@ -1,8 +1,2 @@
-#[macro_use]
 mod headers;
-mod kafka_consumer_record;
-mod kafka_producer_record;
 
-pub use kafka_consumer_record::BorrowedMessageExt;
-pub use kafka_producer_record::EventExt;
-pub use kafka_producer_record::FutureRecordExt;

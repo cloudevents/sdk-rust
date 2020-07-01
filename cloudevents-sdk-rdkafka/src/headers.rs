a0001fa3dac55ffc1044da9bd241ee61abbe0afa
Pranav Bhatt <adpranavb2000@gmail.com>
2020-07-01 10:56:25 +0530

    inprogress commit#1
    Signed-off-by: Pranav Bhatt <adpranavb2000@gmail.com>

--- cloudevents-sdk-rdkafka/src/headers.rs
+++ cloudevents-sdk-rdkafka/src/headers.rs
@@ -1,8 +1,8 @@
 use cloudevents::event::SpecVersion;
 use lazy_static::lazy_static;
+use std::str::FromStr;
 use std::collections::HashMap;
 
-/*
 macro_rules! unwrap_optional_header {
     ($headers:expr, $index:expr) => {
         $headers
@@ -11,36 +11,45 @@ macro_rules! unwrap_optional_header {
             .map(|a| header_value_to_str!(a.1))
     };
 }
-*/
 
 macro_rules! header_value_to_str {
     ($header_value:expr) => {
-        str::from_utf8($header_value).map_err(|e| cloudevents::message::Error::Other {
-            source: Box::new(e),
+        $header_value
+            .to_str()
+            .map_err(|e| cloudevents::message::Error::Other {
+                source: Box::new(e),
+            })
+    };
+}
+
+/*
+macro_rules! str_name_to_header {
+    ($attribute:expr,$value: expr) => {
+        rdkafka::message::OwnedHeaders.add(name: &$attribute, value: &expr).map_err(|e| {
+            cloudevents::message::Error::Other {
+                source: Box::new(e),
+            }
         })
     };
 }
 
+
 macro_rules! attribute_name_to_header {
     ($attribute:expr) => {
-        format!("{}{}", "ce_", $attribute)
+        str_name_to_header!(&["ce-", $attribute].concat())
     };
-}
+} 
 
-fn attributes_to_headers(it: impl Iterator<Item = &'static str>) -> HashMap<&'static str, String> {
+fn attributes_to_headers(
+    it: impl Iterator<Item = &'static str>,
+) -> HashMap<&'static str, HeaderName> {
     it.map(|s| {
         if s == "datacontenttype" {
-            (s, String::from("content-type"))
+            (s, reqwest::header::CONTENT_TYPE)
         } else {
-            (s, attribute_name_to_header!(s))
+            (s, attribute_name_to_header!(s).unwrap())
         }
     })
     .collect()
 }
-
-lazy_static! {
-    pub(crate) static ref ATTRIBUTES_TO_HEADERS: HashMap<&'static str, String> =
-        attributes_to_headers(SpecVersion::all_attribute_names());
-    pub(crate) static ref SPEC_VERSION_HEADER: &'static str = "ce_specversion";
-    pub(crate) static ref CLOUDEVENTS_JSON_HEADER: &'static str = "application/cloudevents+json";
-}
+*/
